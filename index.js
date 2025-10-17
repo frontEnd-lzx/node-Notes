8a42ff1bbfc982c26327bc2585379b8079c9584e
frontEnd-lzx <lzx116383@gmail.com>
2025-10-17 15:10:27 +0800

    feat: 简单的ssr

--- index.js
+++ index.js
@@ -1,5 +1,18 @@
 const fs = require('node:fs')
-const path = require('node:path')
+const { JSDOM } = require('jsdom')
 
+const dom = new JSDOM(`<!DOCTYPE html><div id='app'></div>`)
 
+const document = dom.window.document
 
+fetch('https://api.thecatapi.com/v1/images/search?limit=10&page=1').then(res => res.json()).then(data => {
+    const app = document.getElementById('app')
+    data.forEach(item=>{
+       const img =  document.createElement('img')
+       img.src = item.url
+       img.style.width = '200px'
+       img.style.height = '200px'
+       app.appendChild(img)
+    })
+    fs.writeFileSync('./index.html', dom.serialize())
+})
