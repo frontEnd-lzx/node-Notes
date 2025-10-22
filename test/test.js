df88a1b7d49211e6e70748bae2518692db9bbb65
frontEnd-lzx <lzx116383@gmail.com>
2025-10-22 09:35:27 +0800

    v1.0.2_1

---
+++ test/test.js
@@ -0,0 +1,5 @@
+import { fileURLToPath } from 'node:url';
+import { dirname } from 'node:path';
+const __filename = fileURLToPath(import.meta.url);
+const __dirname  = dirname(__filename);
+console.log("🚀 ~ :85 ~ __dirname:", __dirname)
